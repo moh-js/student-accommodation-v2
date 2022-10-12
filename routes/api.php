8ae6b040bbcf187125c5ccd19dd9284fca99984a
Mohamed Said <201611039>
2022-10-12 11:23:00 +0300

    updates

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\BillingController;
 |
 */
 
-Route::get('/control_number_callback', [BillingController::class, 'receiveControlNumber']);
-Route::get('/payment_callback', [BillingController::class, 'receivePayment']);
+Route::any('/control_number_callback', [BillingController::class, 'receiveControlNumber']);
+Route::any('/payment_callback', [BillingController::class, 'receivePayment']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
